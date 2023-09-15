1b40cb089dd0432af766718428fc2a31daf4c1ba
Pablo Bobarini <50304993+PablissimoB@users.noreply.github.com>
2023-09-14 21:34:05 -0300

    Agregar archivos de proyecto.

---
+++ Clase3/Create.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Clase3
+{
+    public partial class Create : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if(TextBox1.Text != "" && TextBox2.Text != "")
+            {
+                string cadenaConexion = System.Configuration.ConfigurationManager.ConnectionStrings["cadena"].ConnectionString;
+                SqlConnection conexion = new SqlConnection(cadenaConexion);
+                conexion.Open();
+                string addUser = $"insert into usuarios(username, password) values ('{TextBox1.Text}',{TextBox2.Text})";
+                SqlCommand comando = new SqlCommand (addUser, conexion);
+                comando.ExecuteNonQuery();
+                conexion.Close();
+            }
+        }
+    }
+}
